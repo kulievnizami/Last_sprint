51ace0e782abbcdec19e753d636b2ae0d5fc7543
The Overdue Knight <146384015+kulievnizami@users.noreply.github.com>
2026-05-13 18:44:05 +0300

    Первые материалы

--- core/src/main/java/io/github/some_example_name/Main.java
+++ core/src/main/java/io/github/some_example_name/Main.java
@@ -13,14 +13,14 @@ public class Main extends ApplicationAdapter {
     @Override
     public void create() {
         batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        image = new Texture("bg_place.png");
     }
 
     @Override
     public void render() {
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
         batch.begin();
-        batch.draw(image, 140, 210);
+        batch.draw(image, 30, 0);
         batch.end();
     }
 
